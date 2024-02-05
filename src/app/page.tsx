2fc3890d9ecfc072cbb7863c842f41ecee407a9c
Denis S Dujota <denisdujota@gmail.com>
2024-02-05 18:47:50 -0500

    remove dead ref

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import Hero from "../components/sections/hero";
 // import SocialProofSection from "../components/sections/social-proof-section";
 import OurServices from "../components/sections/our-services";
-import PlaceholderImage from "../components/sections/our-services/content";
 import Projects from "../components/sections/projects";
 import MetricsSection from "../components/sections/metrics/metrics-section";
 import AboutSection from "../components/sections/about-section";
